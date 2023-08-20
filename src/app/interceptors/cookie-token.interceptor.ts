5f42d4f86a1ae959903e9543c74ccfd28c755ce4
erick <erick.olviares.gonzalez@gmail.com>
2023-08-20 19:23:51 -0400

    cookie

--- src/app/interceptors/cookie-token.interceptor.ts
+++ src/app/interceptors/cookie-token.interceptor.ts
@@ -20,18 +20,8 @@ export class CookieTokenInterceptor implements HttpInterceptor {
     console.log("Interceptor de cookies")
     // Clonar la solicitud original
     const modifiedRequest = request.clone({
-      setHeaders: {
-        'Content-Type': 'application/json',
-      },
       withCredentials: true
     });
-    this.cookieService.set("prueba","hola soy un token de prueba");
-    
-    if(this.cookieService.check("token")){
-      console.log("Segun esta cosa la cookie no existe, ayuda dios")
-      const token = this.cookieService.get("token")
-      this.cookieService.set("token",token);
-    }  
   
   return next.handle(modifiedRequest);
   }
