205fff999c17b3f22d41da7c162b46d77f7a462f
erick <erick.olviares.gonzalez@gmail.com>
2023-08-20 13:59:00 -0400

    modifica interceptor cookie

--- src/app/interceptors/cookie-token.interceptor.ts
+++ src/app/interceptors/cookie-token.interceptor.ts
@@ -15,14 +15,14 @@ export class CookieTokenInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json' // Establecer el Content-Type a application/json
-      }),
-      withCredentials: true //establecer el traspaso de cookies
-    };
-    const modifiedRequest = request.clone(httpOptions);
-    
-    return next.handle(modifiedRequest);
+  // Clonar la solicitud original
+  const modifiedRequest = request.clone({
+    setHeaders: {
+      'Content-Type': 'application/json'
+    },
+    withCredentials: true
+  });
+  
+  return next.handle(modifiedRequest);
   }
 }
